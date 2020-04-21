2fc9c820866f22c484e136682de82bfca8a198f0
Oscar Amado <oscarfamado@gmail.com>
2020-04-21 17:33:21 -0500

    se cambia la ruta de registro y ademas se modifica el width y height del logo (no de formar una imagen)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-import { HttpClientModule} from '@angular/common/http';
-import { UserService } from './service/user.service';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {Routes, RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {UserService} from './service/user.service';
 
 
-
-import { AppComponent } from './app.component';
-import { RegistroComponent } from './components/registro/registro.component';
-import { LoginComponent } from './components/login/login.component';
-import { UpdateUserComponent } from './components/update-user/update-user.component';
-import { NavComponent } from './components/nav/nav.component';
-import { InicioComponent } from './components/inicio/inicio.component';
-import { NavPeliculasComponent } from './components/nav-peliculas/nav-peliculas.component';
-import { HomePeliculaComponent } from './components/home-pelicula/home-pelicula.component';
-
-
-
-
+import {AppComponent} from './app.component';
+import {RegistroComponent} from './components/registro/registro.component';
+import {LoginComponent} from './components/login/login.component';
+import {UpdateUserComponent} from './components/update-user/update-user.component';
+import {NavComponent} from './components/nav/nav.component';
+import {InicioComponent} from './components/inicio/inicio.component';
+import {NavPeliculasComponent} from './components/nav-peliculas/nav-peliculas.component';
+import {HomePeliculaComponent} from './components/home-pelicula/home-pelicula.component';
 
 
 //rutas
-const appRoutes : Routes =[
-  {path:'inicio',component:InicioComponent},
-  { path: 'registro', component:  RegistroComponent},
-  { path: 'login', component:  LoginComponent},
-
-]
+const appRoutes: Routes = [
+  {path: 'inicio', component: InicioComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomePeliculaComponent},
+];
 
 
 @NgModule({
@@ -40,8 +35,8 @@ const appRoutes : Routes =[
     InicioComponent,
     NavPeliculasComponent,
     HomePeliculaComponent,
-    
-    
+
+
   ],
   imports: [
     BrowserModule,
@@ -54,4 +49,5 @@ const appRoutes : Routes =[
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
