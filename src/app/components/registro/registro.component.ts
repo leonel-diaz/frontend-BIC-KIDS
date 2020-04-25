45cbc4287f9c796f6e7a513d85a0edc6d9e801f5
Oscar Amado <oscarfamado@gmail.com>
2020-04-25 15:10:01 -0500

    Ajuste a los estilos del modulo.

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,38 +1,40 @@
-import { Component, OnInit } from "@angular/core";
-import { User } from "../../models/user";
-import { Router } from "@angular/router";
-import { UserService } from "../../service/user.service";
-import Swal from "sweetalert2";
+import {Component} from '@angular/core';
+import {User} from '../../models/user';
+import {Router} from '@angular/router';
+import {UserService} from '../../service/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
-  selector: "app-registro",
-  templateUrl: "./registro.component.html",
-  styleUrls: ["./registro.component.css"],
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css'],
 })
-export class RegistroComponent implements OnInit {
+export class RegistroComponent {
   public user: User;
 
   constructor(private router: Router, private service: UserService) {
     this.user = new User();
   }
 
-  ngOnInit(): void {}
-
   signUp() {
     this.service.signUp(this.user).subscribe((res: any) => {
-      if (res.statusCode !== 200) {
+      if (res.statusCode === 200) {
         Swal.fire({
-          icon: "error",
-          text: "Usuario no se pudo registrar: ",
+          icon: 'success',
+          text: 'Usuario registrado satisfactoriamente: ' + this.user.name,
         });
+        this.router.navigate(['login']);
       } else {
         Swal.fire({
-          icon: "success",
-          text: "Usuario registrado satisfactoriamente: " + this.user.name,
+          icon: 'error',
+          text: 'Usuario no se pudo registrar.',
         });
-
-        this.router.navigate(["login"]);
       }
+    }, () => {
+      Swal.fire({
+        icon: 'error',
+        text: 'Usuario no se pudo registrar.',
+      });
     });
   }
 }
