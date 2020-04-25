5382fbeb8c09496ff7800db160443a34f6536720
Oscar Amado <oscarfamado@gmail.com>
2020-04-25 15:09:50 -0500

    Ajuste a los estilos del modulo.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from "../../models/user";
-import { UserService } from "../../service/user.service";
-import { Router, RouterLink } from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {User} from "../../models/user";
+import {UserService} from "../../service/user.service";
+import {Router, RouterLink} from "@angular/router";
 import Swal from "sweetalert2";
 
 @Component({
@@ -20,43 +20,48 @@ export class LoginComponent implements OnInit {
     private service: UserService
   ) {
     this.user = new User();
-   }
+  }
 
   ngOnInit(): void {
     this.getLastUser();
   }
+
   getLastUser() {
-    this.lastUser = localStorage.getItem("lastUser");
-    console.log("El último usuario registrado: " + this.lastUser);
+    this.lastUser = localStorage.getItem('lastUser');
+    console.log('El último usuario registrado: ' + this.lastUser);
   }
+
   login() {
-        this.service.login(this.user).subscribe((res: any) => {
-          switch (res.statusCode) {
-            case 400:
-              alert("El usuario no esta registrado");
-              break;
-            case 401:
-              Swal.fire({
-                icon: "error",
-                text: "El usuario no esta registrado ",
-              });
-              break;
-            case 200:
-              Swal.fire({
-                icon: "success",
-                text: "Los datos son correctos ",
-              });
-              localStorage.setItem("user", JSON.stringify(res.user));
-              localStorage.setItem("lastUser", this.lastUser);
-             
-              // this.router.navigate(["dashboard"]);
-              break;
-            default:
-              alert("Error de conexion");
-          }
-        });
+    this.service.login(this.user).subscribe((res: any) => {
+      switch (res.statusCode) {
+        case 400:
+          Swal.fire({
+            icon: 'error',
+            text: 'El usuario no esta registrado ',
+          });
+          break;
+        case 401:
+          Swal.fire({
+            icon: 'error',
+            text: 'El usuario no esta registrado ',
+          });
+          break;
+        case 200:
+          Swal.fire({
+            icon: 'success',
+            text: 'Los datos son correctos ',
+          });
+          localStorage.setItem('user', JSON.stringify(res.user));
+          localStorage.setItem('lastUser', this.lastUser);
+
+          this.router.navigate(['/home']);
+          break;
+        default:
+          alert('Error de conexion');
       }
-    }
+    });
+  }
+}
 
 
 
