be6aae1dd17bb4c0c65d94dc157351338ee4f95e
mayeballen <mayerliballen@gmail.com>
2020-04-29 21:43:34 -0500

    Vista-asociados

--- src/app/components/associates/associates.component.ts
+++ src/app/components/associates/associates.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { url_api } from "../globals/api";
+import { User } from "../../models/user";
+import { UserService } from "../../service/user.service";
+import { Router } from "@angular/router";
+import Swal from "sweetalert2";
+import * as moment from "moment";
 @Component({
-  selector: 'app-associates',
-  templateUrl: './associates.component.html',
-  styleUrls: ['./associates.component.css']
+  selector: "app-associates",
+  templateUrl: "./associates.component.html",
+  styleUrls: ["./associates.component.css"],
 })
 export class AssociatesComponent implements OnInit {
+  public user: User;
+  public url;
+  public image: File;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private service: UserService, private router: Router) {
+    this.url = url_api;
   }
 
+  ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem("user"));
+  }
+  infoAsociado() {}
 }
--- src/app/components/new-user/new-user.component.ts
+++ src/app/components/new-user/new-user.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from "@angular/core";
+import { url_api } from "../globals/api";
+import { User } from "../../models/user";
+import { UserService } from "../../service/user.service";
+import { Router } from "@angular/router";
+import Swal from "sweetalert2";
+import * as moment from "moment";
 
 interface HTMLInputEvent extends Event {
   target: HTMLInputElement & EventTarget;
@@ -9,23 +15,64 @@ interface HTMLInputEvent extends Event {
   styleUrls: ["./new-user.component.css"],
 })
 export class NewUserComponent implements OnInit {
-  file: File;
-  photoSelected: String | ArrayBuffer;
+  public user: User;
+  public url;
+  public image: File;
 
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  onPhotoSelected(event: HTMLInputEvent): void {
-    if (event.target.files && event.target.files[0]) {
-      this.file = event.target.files[0];
-      //images
-      const reader = new FileReader();
-      reader.onload = (e) => (this.photoSelected = reader.result);
-      reader.readAsDataURL(this.file);
-    }
+  constructor(private service: UserService, private router: Router) {
+    this.url = url_api;
   }
-  uploadPhoto(nombre: HTMLInputElement) {
-    console.log(nombre.value);
+
+  ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem("user"));
+  }
+
+  updateUserAssociate() {
+    console.log(this.user);
+    this.service.updateUser(this.user).subscribe((res: any) => {
+      switch (res.statusCode) {
+        case 500:
+          Swal.fire({
+            icon: "error",
+            text: "Error en el servidor",
+          });
+          break;
+        case 400:
+          Swal.fire({
+            icon: "error",
+            text: "Error al modificar el usuario",
+          });
+          console.log(res.statusCode);
+          break;
+        case 200:
+          Swal.fire({
+            icon: "success",
+            text: "Asociado actualizado correctamente",
+          });
+
+          this.service
+            .saveImage(this.image, this.user._id)
+            .subscribe((res: any) => {
+              this.user.image = res.image;
+              res.dataUser.image = this.user;
+              localStorage.setItem("dataUser", JSON.stringify(res.dataUser));
+            });
+          console.log("----> ", res.dataUser);
+          localStorage.setItem("user", JSON.stringify(res.dataUser));
+          this.router.navigate(["/associates"]);
+          break;
+        default:
+          Swal.fire({
+            icon: "error",
+            text: "Algo salió mal :(",
+          });
+      }
+    });
+  }
+
+  loadImage(image: any) {
+    /*console.log('target => ', image.target);
+    console.log('image.target.files[0] => ', image.target.files[0]);*/
+    this.image = image.target.files[0] as File;
   }
 }
